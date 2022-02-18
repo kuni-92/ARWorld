2b9d264363fc385ce9896c9113411bf4857651b8
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2022-02-18 21:09:29 +0900

    Initial Commit

---
+++ ARWorld/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  ARWorld
+//  
+//  Created by kunikuni03 on 2022/02/18
+//  
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
