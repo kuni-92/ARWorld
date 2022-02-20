15eafc8ae42f02719c47ff83e6faebf12a18cf3a
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2022-02-20 21:03:38 +0900

    add AR object

--- ARWorld/ContentView.swift
+++ ARWorld/ContentView.swift
@@ -23,11 +23,9 @@ struct ARViewContainer: UIViewRepresentable {
         
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
-        let box2Anchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
-        arView.scene.anchors.append(box2Anchor)
         
         return arView
         
